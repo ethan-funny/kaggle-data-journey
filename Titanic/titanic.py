d940aeb4d783f34136cb9031b8f8b72bfeeb2c11
ethan <shuyouxin1@gmail.com>
2017-05-18 18:04:04 +0800

    code refactor

--- Titanic/titanic.py
+++ Titanic/titanic.py
@@ -5,12 +5,23 @@ import csv as csv
 
 import numpy as np
 import matplotlib.pyplot as plt
+from IPython.display import display
 
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import VotingClassifier
+from sklearn.ensemble import BaggingRegressor
+
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import train_test_split
+
+
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import recall_score
 
 
 class Titanic(object):
@@ -18,7 +29,7 @@ class Titanic(object):
         self.train = pd.read_csv(train_file)
         self.test = pd.read_csv(test_file)
 
-    def data_clean(self):
+    def data_preprocess(self):
         """
         data preprocess
         """
@@ -45,8 +56,8 @@ class Titanic(object):
         self.test["Age"] = self.test["Age"].astype(int)
 
         # drop unnecessary columns
-        self.train = self.train.drop(['PassengerId', 'Name', 'Ticket', 'Cabin'], axis=1)
-        self.test = self.test.drop(['Name', 'Ticket', 'Cabin'], axis=1)
+        # self.train = self.train.drop(['PassengerId', 'Name', 'Ticket', 'Cabin'], axis=1)
+        # self.test = self.test.drop(['Name', 'Ticket', 'Cabin'], axis=1)
 
 
     def feature_engineering(self):
@@ -88,8 +99,11 @@ class Titanic(object):
         pclass_dummies_test = pd.get_dummies(self.test['Pclass'], prefix='Pclass')
 
         # feature: Sex
-        self.train['Sex'] = self.train['Sex'].map({'female': 0, 'male': 1}).astype(int)
-        self.test['Sex'] = self.test['Sex'].map({'female': 0, 'male': 1}).astype(int)
+        # self.train['Sex'] = self.train['Sex'].map({'female': 0, 'male': 1}).astype(int)
+        # self.test['Sex'] = self.test['Sex'].map({'female': 0, 'male': 1}).astype(int)
+
+        gender_dummies_train = pd.get_dummies(self.train['Sex'], prefix='Gender')
+        gender_dummies_test = pd.get_dummies(self.test['Sex'], prefix='Gender')
 
         # feature: Embarked
         embarked_dummies_train = pd.get_dummies(self.train['Embarked'], prefix='Embarked')
@@ -106,6 +120,9 @@ class Titanic(object):
         self.train['IsAlone'] = family_size.apply(lambda x: 1 if x == 0 else 0)
         self.test['IsAlone'] = family_size.apply(lambda x: 1 if x == 0 else 0)
 
+        isalone_dummies_train = pd.get_dummies(self.train['IsAlone'], prefix='IsAlone')
+        isalone_dummies_test = pd.get_dummies(self.test['IsAlone'], prefix='IsAlone')
+
         # feature: fare
         self.train['Fare'] = self.train['Fare'].apply(get_fare_level)
         self.test['Fare'] = self.test['Fare'].apply(get_fare_level)
@@ -114,46 +131,210 @@ class Titanic(object):
 
 
         # concat
-        self.X_train = pd.concat([self.train[['Sex', 'IsAlone']], pclass_dummies_train,
-                                  person_dummies_train, fare_dummies_train, embarked_dummies_train],
+        self.X_train = pd.concat([gender_dummies_train, pclass_dummies_train,
+                                  person_dummies_train, embarked_dummies_train],
                                  axis=1)
         self.Y_train = self.train['Survived']
-        self.X_test = pd.concat([self.test[['Sex', 'IsAlone']], pclass_dummies_test,
-                                 person_dummies_test, fare_dummies_test,
+        self.X_test = pd.concat([gender_dummies_test, pclass_dummies_test,
+                                 person_dummies_test,
                                  embarked_dummies_test], axis=1)
+
+        # split_train, split_validation = train_test_split(train, test_size = 0.3, random_state = 0)
 
 
         # feature selection
 
-
-    def train(self):
+    @staticmethod
+    def model_train(x_train, y_train):
         logreg = LogisticRegression(C=1.0, penalty='l1', tol=1e-6)
-        logreg.fit(self.X_train, self.Y_train)
-        self.Y_pred = logreg.predict(self.X_test)
-        logreg.score(self.X_train, self.Y_train)
+        logreg.fit(x_train, y_train)
+        logreg.score()
 
-        coeff_df = pd.DataFrame({
-            'Features': self.X_train.columns,
-            'Coefficient': pd.Series(logreg.coef_[0])
-        })
+        # coeff_df = pd.DataFrame({
+        #     'Features': self.X_train.columns,
+        #     'Coefficient': pd.Series(self.logreg.coef_[0])
+        # })
+        #
+        # print(coeff_df)
+        return logreg
 
 
     def predict(self):
+        self.Y_pred = self.logreg.predict(self.X_test)
+
+    @staticmethod
+    def lr(x_train, y_train, x_test, y_test, features):
+        logreg = LogisticRegression(C=1.0, penalty='l1', tol=1e-6)
+        logreg.fit(x_train, y_train)
+
+        y_pred = logreg.predict(x_test)
+        coeff = logreg.coef_[0]
+
+        cmatrix = confusion_matrix(y_test, y_pred)
+        auc = roc_auc_score(y_test, y_pred)
+        recall = recall_score(y_test, y_pred)
+        accuracy = accuracy_score(y_test, y_pred)
+
+        coeff_df = pd.DataFrame({
+            'Coefficient': pd.Series(coeff),
+            'Features': features
+        })
+        score_df = pd.DataFrame(
+            [[auc, accuracy, recall]],
+            columns=['auc', 'accuracy', 'recall']
+        )
+        cmatrix_df = pd.DataFrame(
+            cmatrix,
+            columns=['0(predict)', '1(predict)'],
+            index=['0(actual)', '1(actual)']
+        )
+
+        return dict(model=logreg, y_pred=y_pred, coeff=coeff_df, cmatrix=cmatrix_df, score=score_df)
+
+    def lr_experiment(self, train_data):
+
+        split_train, split_cv = train_test_split(train_data, test_size=0.3,
+                                                 random_state=0)
+
+        result = self.lr(
+            split_train.as_matrix()[:, 1:],
+            split_train.as_matrix()[:, 0],
+            split_cv.as_matrix()[:, 1:],
+            split_cv.as_matrix()[:, 0],
+            split_cv.columns[1:]
+        )
+
+        # result_ensemble = self.ensemble(
+        #     split_train.as_matrix()[:, 1:],
+        #     split_train.as_matrix()[:, 0],
+        #     split_cv.as_matrix()[:, 1:],
+        #     split_cv.as_matrix()[:, 0]
+        # )
+
+        print('====score====')
+        display(result['score'])
+
+        # print('====ensemble score====')
+        # display(result_ensemble['score'])
+
+        print('===confusion matrix====')
+        display(result['cmatrix'])
+
+        # print('===ensemble confusion matrix====')
+        # display(result_ensemble['cmatrix'])
+
+        print('=====coeff====')
+        display(result['coeff'].transpose())
+
+        return result,
+
+
+    def evaluate(self):
+        print("score:{}".format(self.logreg.score(self.X_train, self.Y_train)))
+
+
+    @staticmethod
+    def ensemble(x_train, y_train, x_test, y_test):
+        clf = LogisticRegression(C=1.0, penalty='l1', tol=1e-6)
+        bagging_clf = BaggingRegressor(clf, n_estimators=20, max_samples=0.8,
+                                       max_features=1.0, bootstrap=True,
+                                       bootstrap_features=False, n_jobs=-1)
+        bagging_clf.fit(x_train, y_train)
+
+        y_pred_f = bagging_clf.predict(x_test)
+
+        y_pred = y_pred_f.astype(np.int32)
+
+        cmatrix = confusion_matrix(y_test, y_pred)
+        auc = roc_auc_score(y_test, y_pred)
+        recall = recall_score(y_test, y_pred)
+        accuracy = accuracy_score(y_test, y_pred)
+
+        score_df = pd.DataFrame(
+            [[auc, accuracy, recall]],
+            columns=['auc', 'accuracy', 'recall']
+        )
+        cmatrix_df = pd.DataFrame(
+            cmatrix,
+            columns=['0(predict)', '1(predict)'],
+            index=['0(actual)', '1(actual)']
+        )
+
+        return dict(model=bagging_clf, y_pred=y_pred, cmatrix=cmatrix_df, score=score_df)
+
+    @staticmethod
+    def rf(x_train, y_train, x_test, y_test):
+        random_forest = RandomForestClassifier(n_estimators=100)
+
+        random_forest.fit(x_train, y_train)
+
+        y_pred = random_forest.predict(x_test)
+
+        cmatrix = confusion_matrix(y_test, y_pred)
+        auc = roc_auc_score(y_test, y_pred)
+        recall = recall_score(y_test, y_pred)
+        accuracy = accuracy_score(y_test, y_pred)
+
+        score_df = pd.DataFrame(
+            [[auc, accuracy, recall]],
+            columns=['auc', 'accuracy', 'recall']
+        )
+        cmatrix_df = pd.DataFrame(
+            cmatrix,
+            columns=['0(predict)', '1(predict)'],
+            index=['0(actual)', '1(actual)']
+        )
+
+        return dict(model=random_forest, y_pred=y_pred, cmatrix=cmatrix_df,
+                    score=score_df)
+
+
+
+    @staticmethod
+    def generate_result(model, test_data, passenger_id):
+        y_pred = model.predict(test_data)
+
+        result = pd.DataFrame({
+            'PassengerId': passenger_id,
+            'Survived': y_pred
+        })
+
+        result.to_csv('titanic_solution.csv', index=False)
+
+
+    def tocsv(self):
         result = pd.DataFrame({
             'PassengerId': self.test['PassengerId'],
             'Survived': self.Y_pred
         })
-
-    def evaluate(self):
-        pass
-
-
-    def ensemble(self):
+        result.to_csv('titanic_solution.csv', index=False)
 
 
 
+if __name__ == '__main__':
+    titanic = Titanic('train.csv', 'test.csv')
+    print('data clean...')
+    titanic.data_clean()
+
+    print('feature engineering...')
+    titanic.feature_engineering()
+
+    print('training...')
+    titanic.model_train()
+
+    print('predict...')
+    titanic.predict()
+
+    print('to csv...')
+    titanic.tocsv()
+
+    print('evaluating...')
+    titanic.evaluate()
 
 
+
+# 特征交叉
+# 其他特征
 
 # https://www.kaggle.com/arthurtok/titanic/introduction-to-ensembling-stacking-in-python
 # http://localhost:6768/notebooks/A-Journey-through-Titanic.ipynb
